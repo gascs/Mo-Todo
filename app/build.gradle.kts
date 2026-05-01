44ef6bd708f992e02fed6128418e5710992d9bd9
gascs <linpo1438562376@gmail.com>
2026-05-01 10:09:32 +0800

    fix: versionCode升至4，修复F-Droid版本号倒退问题

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.mo.todo"
         minSdk = 34
         targetSdk = 35
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
